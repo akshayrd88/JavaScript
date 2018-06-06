956849c1e87bcd6c405a931d790810c159ad3970
akshayrd88 <39278707+akshayrd88@users.noreply.github.com>
2018-06-06 16:02:15 -0400

    Update UserValidation.js

--- UserValidation.js
+++ UserValidation.js
@@ -1,16 +1,53 @@
 var users= [];
 
+function usertable(){
+    if(localStorage.allusers){
+        users= JSON.parse(localStorage.allusers);
+    }
+    var table= '<table>';
+    table += '<th>username</th><th>password</th><th>company</th><th>email</th><th>location</th><th>gender</th>';
+    for(var i = 0; i<users.length; i++){
+		table += '<tr><td>'+users[i].username+'</td><td>'+users[i].pword+'</td><td>'+users[i].email+'</td><td>'+users[i].location+'</td><td>'+users[i].company+'</td><td>'+users[i].gender+'</td></tr>';
+    }
+    table += '</table>';
+	document.getElementById('finaltable').innerHTML = table;
+}
+
+function reset(){
+    document.getElementById('input').reset();
+}
+
 document.getElementById('btn').addEventListener('click',function(){
     var user={
-        'username': document.getElementById('fName').value,
-        'lastname': document.getElementById('lName').value,
-        'organisation': document.getElementById('org').value,
+        'username': document.getElementById('UName').value,
+        'password': document.getElementById('pwd').value,
+        'company': document.getElementById('company').value,
         'email': document.getElementById('email').value,
         'location': document.getElementById('loc').value,
-        'gender': document.getElementById('female').value,
-        'gender': document.getElementById('male').value
+        'gender': document.querySelector('input[name="gender"]:checked')? document.querySelector('input[name="gender"]:checked').value: ''
     };
+    if((user.username) && (user.password) && (user.email) && (user.company) && (user.location) && (user.gender)){
     users.push(user);
     localStorage.allusers= JSON.stringify(users);
+    }
+    if(user.username == ""){
+        document.getElementById('unameerr').innerHTML= "Enter a Username, Try again!!"
+    }
+    if(user.password == ""){
+        document.getElementById('pwderr').innerHTML= "Enter a Username, Try again!!"
+    }
+    if(user.company == ""){
+        document.getElementById('comerr').innerHTML= "Enter a Username, Try again!!"
+    }
+    if(user.email == ""){
+        document.getElementById('emailerr').innerHTML= "Enter a Username, Try again!!"
+    }
+    if(user.location == ""){
+        document.getElementById('locerr').innerHTML= "Enter a Username, Try again!!"
+    }
+
+usertable();
+reset();
+    
 });
 
