93af9d1198ce5c47280c8514939f8bd9199f5491
akshayrd88 <39278707+akshayrd88@users.noreply.github.com>
2018-07-11 11:01:15 -0400

    Update textbox.js

--- textbox.js
+++ textbox.js
@@ -1,39 +1,38 @@
 //forward
+var fwdcount= 0;
+
 document.getElementById('reflect').addEventListener('click', function(){
-    var str= document.getElementById('myText').value;
     
-        window.a=setInterval(function(){  
-            if(str.length==0){
-                clearInterval(window.a);
-                console.log('cleared');
-            }
-            document.getElementById('myFeed').value += str.slice(0, 1);
-            document.getElementById('myText').value=str.replace(str.slice(0, 1), "");
-            str = str.replace(str.slice(0, 1), "");
-            console.log(str);        
-        }, 1000);
-                
-    //}
+    float = setInterval(function(){
+        document.querySelector("#box2").value += document.querySelector("#box1").value.charAt(0);
+        document.querySelector("#box1").value = document.querySelector("#box1").value.substr(1);
+        if(document.querySelector("#box1").value == 0){
+            clearInterval(float);
+        }
+    }
+    ,1000) 
 });
+
 
 //backward
 
 document.getElementById('revert').addEventListener('click', function(){
-    var str2= document.getElementById('myFeed').value;
-     window.b=setInterval(function(){
-        if(str2.length==0){
-            clearInterval(window.b);
-            console.log('cleared');
-        }      
-        document.getElementById('myText').value += str2.slice(0, 1);
-        document.getElementById('myFeed').value=str2.replace(str2.slice(0, 1), "");
-        str2 = str2.replace(str2.slice(0, 1), "");
-        console.log(str2);
-    }, 1000);
+
+clearInterval(float);
+    float = setInterval(function(){
+        document.querySelector("#box1").value = document.querySelector("#box2").value.charAt(document.querySelector("#box2").value.length-1)+document.querySelector("#box1").value;
+        document.querySelector("#box2").value = document.querySelector("#box2").value.substr(0,document.querySelector("#box2").value.length-1);
+        if(document.querySelector("#box2")==0){
+            clearInterval(float);
+        }
+    },1000)
 });
 
+
+//pause
+
 document.getElementById('pause').addEventListener('click', function(){
-    clearTimeout(window.b);
-    clearTimeout(window.a);
+    
+    clearInterval(float);
 });
 
